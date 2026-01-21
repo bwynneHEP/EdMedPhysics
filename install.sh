8d5515a9de54c0cc50503be0d37548c188a48cdd
Ben Wynne <bwynne@phcompute021.ph.ed.ac.uk>
2026-01-21 14:11:54 +0000

    Tidier build

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 # script to automate the installation of
 # the EdMedPhys project Geant4 executable
 
-git clone https://github.com/bwynneHEP/EdMedPhysics.git
-cd EdMedPhysics
-source set_environment.sh
+git clone https://github.com/bwynneHEP/EdMedPhysics
+source EdMedPhysics/set_environment.sh
 mkdir build
 cd build
-cmake ..
+cmake ../EdMedPhysics
 make
